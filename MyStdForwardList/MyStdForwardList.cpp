05503f606451ff856566acc7c18ef11e40deef78
Данило <@LAPTOP-7M1VSVI1>
2025-11-21 23:11:03 +0200

    Add remaining methods(WIP: resize()). Create std::list prototype.

--- MyStdForwardList/MyStdForwardList.cpp
+++ MyStdForwardList/MyStdForwardList.cpp
@@ -13,13 +13,19 @@ namespace my_std {
         Node<T>* head;
         size_t size;
 
-        Node<T>* find_Node(Node<T>* needed_Node) {
+        /*Node<T>* find_Node(Node<T>* needed_Node) {
             for (Node<T>* current = head; current != nullptr; current = current->next) {
                 if (current == needed_Node) {
                     return current;
                 }
             }
             return nullptr;
+        }*/
+
+        friend void swap(MyForwardList& first, MyForwardList& second) {
+            using std::swap;
+            swap(first.head, second.head);
+            swap(first.size, second.size);
         }
     public:
         MyForwardList() : head(nullptr), size(0) {}
@@ -39,6 +45,34 @@ namespace my_std {
                 size++;
             }
         }
+        MyForwardList(MyForwardList& other) : head(nullptr), size(other.size) {
+            Node<T>* current = nullptr;
+            for (Node<T>* other_current = other.head; other_current != nullptr; other_current = other_current->next) {
+                Node<T>* newNode = new Node<T>{ other_current->data, nullptr };
+                if (!head) {
+                    head = newNode;
+                    current = newNode;
+                }
+                else {
+                    current->next = newNode;
+                    current = newNode;
+                }
+            }
+            display();
+        }
+        MyForwardList(MyForwardList&& other) noexcept {
+            head = other.head;
+            size = other.size;
+            other.head = nullptr;
+            other.size = 0;
+            display();
+        }
+        MyForwardList& operator=(MyForwardList other) {
+            using std::swap;
+            swap(*this, other);
+            display();
+            return *this;
+        }
         ~MyForwardList() {
             clear();
         }
@@ -54,6 +88,15 @@ namespace my_std {
             else {
                 return;
             }
+        }
+
+        static void ruleoffive_list_demo() {
+            my_std::MyForwardList<int> forward_list = { 0, 1, 1, 2, 3, 5, 8, 13, 21 };
+            my_std::MyForwardList<int> forward_list_c_copy(forward_list);
+            my_std::MyForwardList<int> forward_list_c_move = std::move(forward_list_c_copy);
+            my_std::MyForwardList<int> forward_list_op_cas;
+            forward_list_op_cas = forward_list_c_move;
+            std::cout << "" << std::endl;
         }
 
         // O(N)
@@ -80,23 +123,6 @@ namespace my_std {
             head = newNode;
             display();
         }
-        void insert_after() {
-
-        }
-        void erase_after() {
-
-        }
-        void resize(const size_t size) {
-            if (this->size < size) {
-                find_Node();
-            }
-            else if (this->size > size) {
-                find_Node();
-            }
-            else {
-                return;
-            }
-        }
         // O(1)
         void pop_front() {
             if (head != nullptr) {
@@ -104,6 +130,54 @@ namespace my_std {
                 Node<T>* new_head = head->next;
                 head = new_head;
                 delete old_head;
+                display();
+            }
+            else {
+                return;
+            }
+        }
+        // O(1)
+        void insert_after(Node<T>* it, const T& val) {
+            Node<T>* newNode_for_next = it->next;
+            Node<T>* newNode = new Node<T>{ val, newNode_for_next };
+            it->next = newNode;
+            display();
+        }
+        // O(1)
+        void erase_after(Node<T>* it) {
+            Node<T>* oldNode = it->next;
+            it->next = oldNode->next;
+            delete oldNode;
+            display();
+        }
+        void resize(const size_t size) {
+            size_t old_size = this->size;
+            size_t new_size = size;
+            if (old_size < new_size) {
+                Node<T>* current = head;
+                while (current->next != nullptr) {
+                    current = current->next;
+                }
+                for (size_t i = old_size; i < new_size; i++) {
+                    Node<T>* newNode = new Node<T>{ 0, nullptr };
+                    current->next = newNode;
+                    current = newNode;
+                }
+            }
+            else if (old_size > new_size) {
+                Node<T>* current = head;
+                for (size_t i = 0; i < new_size - 1; i++) {
+                    current = current->next;
+                }
+                Node<T>* current_for_next = nullptr;
+                while (current->next != nullptr) {
+                    current_for_next = current->next;
+                    delete current;
+                    current = current_for_next;
+                }
+            }
+            else if (new_size == 0) {
+                clear();
             }
             else {
                 return;
@@ -123,20 +197,50 @@ namespace my_std {
         bool empty() {
             return !head;
         }
+        Node<T>* begin() {
+            return head;
+        }
+
+        // Note: std::forward_list does not provide push_back() and pop_back() because it`s O(N),
+        // while std::vector has O(1) for this. 
     };
 }
 
 int main()
 {
+    my_std::MyForwardList<int>::ruleoffive_list_demo();
+
     my_std::MyForwardList<int> forward_list;
     forward_list.display();
     std::cout << "" << std::endl;
-    my_std::MyForwardList<int> init_forward_list = {11, 22, 45, 32};
+    my_std::MyForwardList<int> init_forward_list = {11, 22, 45, 32, 2025};
     std::cout << "Initializing a new forward list." << std::endl;
     init_forward_list.display();
     std::cout << "my_std::MyForwardList.push_front()" << std::endl;
     init_forward_list.push_front(404);
     std::cout << "my_std::MyForwardList.pop_front()." << std::endl;
     init_forward_list.pop_front();
+    std::cout << "my_std::MyForwardList.insert_after()." << std::endl;
+    auto it_insert = init_forward_list.begin();
+    init_forward_list.insert_after(it_insert, 123);
+    std::cout << "my_std::MyForwardList.erase_after()." << std::endl;
+    auto it_erase = init_forward_list.begin();
+    init_forward_list.erase_after(it_erase);
+    std::cout << "my_std::MyForwardList.resize() (to bigger size)." << std::endl;
+    init_forward_list.resize(10);
+    std::cout << "my_std::MyForwardList.resize() (to smaller size)." << std::endl;
+    init_forward_list.resize(3);
+    std::cout << "my_std::MyForwardList.clear()." << std::endl;
+    init_forward_list.clear();
+    init_forward_list.display();
+    std::cout << "my_std::MyForwardList.empty()." << std::endl;
+    if(init_forward_list.empty()){
+        std::cout << "forward list is empty." << std::endl;
+    }
+    else {
+        std::cout << "forward list is NOT empty." << std::endl;
+    }
+    
+
     return 0;
 }
---
+++ MyStdList/MyStdList.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <initializer_list>
+
+namespace my_std {
+    template<typename T>
+    class MyList {
+    private:
+        template<typename T>
+        struct Node {
+            T data;
+            Node<T>* next;
+            Node<T>* prev;
+        };
+        Node<T>* head;
+        Node<T>* tail;
+        size_t size;
+
+        friend void swap(MyList& first, MyList& second) {
+            using std::swap;
+            swap(first.head, second.head);
+            swap(first.tail, second.tail);
+            swap(first.size, second.size);
+        }
+    public:
+        MyList() : head(nullptr), tail(nullptr), size(0) {}
+        MyList(std::initializer_list<T> list) {}
+        MyList& operator=(MyList other) {
+            using std::swap;
+            swap(*this, other);
+            display();
+            return *this;
+        }
+        ~MyList() {
+            // clear();
+        }
+        void clear() {}
+
+        /*static void ruleoffive_list_demo() {
+            my_std::MyList<int> list = { 0, 1, 1, 2, 3, 5, 8, 13, 21 };
+            my_std::MyList<int> list_c_copy(list);
+            my_std::MyList<int> list_c_move = std::move(list_c_copy);
+            my_std::MyList<int> list_op_cas;
+            my_std::MyList<int> list_c_move;
+            std::cout << "" << std::endl;
+        }*/
+
+        T& operator[](const size_t i) {}
+        void push_front() {}
+        void pop_front() {}
+        // push_back() and pop_back() in std::list have O(1) while std::forward_list - 0(N)
+        void push_back() {}
+        void pop_back() {}
+        void insert_after() {}
+        void erase_after() {}
+
+        bool empty() {
+            return !head;
+        }
+
+        void display() {}
+
+    };
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
