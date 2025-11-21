adc89ddc50fdd05ede3ec695d39cd1fe507952a5
Данило <@LAPTOP-7M1VSVI1>
2025-11-21 13:46:29 +0200

    Add push_front(), pop_front(), clear(), private "find_Node()". WIP: resize(), insert_after(), erase_after().

--- MyStdForwardList/MyStdForwardList.cpp
+++ MyStdForwardList/MyStdForwardList.cpp
@@ -5,13 +5,22 @@ namespace my_std {
     template<typename T>
     class MyForwardList {
     private:
+        template<typename T>
         struct Node {
             T data;
-            Node* next;
+            Node<T>* next;
         };
-        Node* head;
+        Node<T>* head;
         size_t size;
 
+        Node<T>* find_Node(Node<T>* needed_Node) {
+            for (Node<T>* current = head; current != nullptr; current = current->next) {
+                if (current == needed_Node) {
+                    return current;
+                }
+            }
+            return nullptr;
+        }
     public:
         MyForwardList() : head(nullptr), size(0) {}
 
@@ -31,8 +40,22 @@ namespace my_std {
             }
         }
         ~MyForwardList() {
-            
+            clear();
         }
+        void clear() {
+            if (head != nullptr) {
+                Node<T>* current_for_next = nullptr;
+                for (Node<T>* current = head; current != nullptr; current = current_for_next) {
+                    current_for_next = current->next;
+                    delete current;
+                    size--;
+                }
+            }
+            else {
+                return;
+            }
+        }
+
         // O(N)
         T& operator[](const size_t i) {
             size_t count = 0;
@@ -52,29 +75,53 @@ namespace my_std {
             return current->data;
         }
         // O(1)
-        void insertAtBeginning() {
+        void push_front(const T& data) {
+            Node<T>* newNode = new Node<T>{ data, head };
+            head = newNode;
+            display();
+        }
+        void insert_after() {
 
         }
-        void insertAfter() {
+        void erase_after() {
 
         }
-        // O(1)
-        void deleteAtBeginning() {
-
-        }
-        void deleteAtEnd() {
-
-        }
-        void display() {
-            for (Node<T>* current = head; current != nullptr; current = current->next) {
-                std::cout << current->data << std::endl;
+        void resize(const size_t size) {
+            if (this->size < size) {
+                find_Node();
+            }
+            else if (this->size > size) {
+                find_Node();
+            }
+            else {
+                return;
             }
         }
-        bool isEmpty() {
-            return !head;
+        // O(1)
+        void pop_front() {
+            if (head != nullptr) {
+                Node<T>* old_head = head;
+                Node<T>* new_head = head->next;
+                head = new_head;
+                delete old_head;
+            }
+            else {
+                return;
+            }
         }
-        Node* getHead() {
-            return head;
+        void display() {
+            if (empty()) {
+                std::cout << "There are no elements in the list." << std::endl;
+            }
+            else {
+                for (Node<T>* current = head; current != nullptr; current = current->next) {
+                    std::cout << current->data << ". ";
+                }
+                std::cout << "" << std::endl;
+            }
+        }
+        bool empty() {
+            return !head;
         }
     };
 }
@@ -82,7 +129,14 @@ namespace my_std {
 int main()
 {
     my_std::MyForwardList<int> forward_list;
+    forward_list.display();
+    std::cout << "" << std::endl;
     my_std::MyForwardList<int> init_forward_list = {11, 22, 45, 32};
-
+    std::cout << "Initializing a new forward list." << std::endl;
+    init_forward_list.display();
+    std::cout << "my_std::MyForwardList.push_front()" << std::endl;
+    init_forward_list.push_front(404);
+    std::cout << "my_std::MyForwardList.pop_front()." << std::endl;
+    init_forward_list.pop_front();
     return 0;
 }
