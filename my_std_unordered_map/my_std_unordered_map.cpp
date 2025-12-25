5ef3ee4481eea51fb0487056ab5e3908dcb1bf6c
Данило <@LAPTOP-7M1VSVI1>
2025-12-25 15:41:47 +0200

    feat complete my_std_unordered_map project: - Implement rule of five; - Implement basic STL-methods; - Implment class Iterator;

--- my_std_unordered_map/my_std_unordered_map.cpp
+++ my_std_unordered_map/my_std_unordered_map.cpp
@@ -1,5 +1,11 @@
 ﻿#include <iostream>
 #include <utility>
+#include <functional>
+
+template<typename Key, typename Value>
+std::ostream& operator<<(std::ostream& os, const std::pair<const Key, Value>& pair) {
+    return os << pair.first << ": " << pair.second << ";" << std::endl;
+}
 
 namespace my_std {
     template<typename Key, typename Value>
@@ -16,11 +22,16 @@ namespace my_std {
         size_t hm_size;
         size_t hm_capacity;
 
+        friend void swap(Hashmap& first, Hashmap& second) {
+            using std::swap;
+            swap(first.buckets, second.buckets);
+            swap(first.hm_size, second.hm_size);
+            swap(first.hm_capacity, second.hm_capacity);
+        }
 
 
-        size_t hash_function(Key bucket_key) {
-            std::hash<Key> hash;
-            return hash(bucket_key) % hm_capacity;
+        size_t hash_function(const Key& bucket_key) const {
+            return std::hash<std::remove_const_t<Key>>{}(bucket_key) % hm_capacity;
         }
 
 
@@ -70,25 +81,41 @@ namespace my_std {
         }
     public:
         Hashmap() : hm_capacity(3), hm_size(0) {
-            buckets = new Bucket * [hm_capacity]();
+            buckets = new Bucket*[hm_capacity]();
         }
         Hashmap(std::initializer_list<value_type> hm) : hm_capacity(3), hm_size(0) {
             buckets = new Bucket * [hm_capacity]();
-            for (const auto& [key, value] : hm) {
-                insert({ key, value });
+            for (const auto& item : hm) {
+                insert(item);
             }
         }
-        ~Hashmap() {
-            Bucket* current_for_next = nullptr;
-            for (int i = 0; i < hm_capacity; i++) {
-                for (Bucket* current = buckets[i]; current != nullptr; current = current_for_next) {
-                    current_for_next = current->next;
-                    delete current;
+        Hashmap(const Hashmap& other) : hm_capacity(other.hm_capacity), hm_size(0) {
+            this->buckets = new Bucket * [this->hm_capacity]();
+            for (size_t i = 0; i < other.hm_capacity; i++) {
+                Bucket* current = other.buckets[i];
+                while (current != nullptr) {
+                    this->insert(current->data);
+                    current = current->next;
                 }
             }
-            delete[] buckets;
-            hm_capacity = 0;
-            hm_size = 0;
+        }
+        Hashmap(Hashmap&& other) noexcept {
+            this->buckets = other.buckets;
+            this->hm_size = other.hm_size;
+            this->hm_capacity = other.hm_capacity;
+            other.buckets = nullptr;
+            other.hm_size = 0;
+            other.hm_capacity = 0;
+        }
+
+        Hashmap& operator=(Hashmap other) {
+            using std::swap;
+            swap(*this, other);
+            return *this;
+        }
+
+        ~Hashmap() {
+            clear();
         }
 
         void insert(const value_type& pair) {
@@ -132,15 +159,138 @@ namespace my_std {
                 }
             }
         }
-        Bucket* search(const Key& arg_key) {
-            Bucket* current = buckets[hash_function(arg_key)];
+
+
+
+        class Iterator {
+        private:
+            friend class Hashmap;
+            using value_type = std::pair<const Key, Value>;
+            Bucket* ptr;
+            Bucket** buckets_ptr;
+            size_t bucket_index; 
+            size_t capacity;
+            Iterator(Bucket* node, Bucket** b_ptr, size_t idx, size_t cap)
+                : ptr(node), buckets_ptr(b_ptr), bucket_index(idx), capacity(cap) {
+            }
+
+        public:
+            Iterator() : ptr(nullptr), buckets_ptr(nullptr), bucket_index(0), capacity(0) {}
+
+            Iterator& operator++() {
+                if (ptr != nullptr) {
+                    ptr = ptr->next;
+                }
+                while (ptr == nullptr && ++bucket_index < capacity) {
+                    ptr = buckets_ptr[bucket_index];
+                }
+
+                return *this;
+            }
+
+            bool operator==(const Iterator& other) const {
+                return ptr == other.ptr;
+            }
+
+            bool operator!=(const Iterator& other) const {
+                return ptr != other.ptr;
+            }
+
+            value_type& operator*() const {
+                return ptr->data;
+            }
+
+            value_type* operator->() const {
+                return &(ptr->data);
+            }
+        };
+
+
+
+        Iterator search(const Key& key) {
+            size_t index = hash_function(key);
+            Bucket* current = buckets[index];
+
             while (current != nullptr) {
-                if (current->data.first == arg_key) {
-                    return current;
+                if (current->data.first == key) {
+                    return Iterator(current, buckets, index, hm_capacity);
                 }
                 current = current->next;
             }
-            return nullptr;
+            return end();
+        }
+        Value& operator[](const Key& key) {
+            if (contains(key)) {
+                return (*find(key)).second;
+            }
+            else {
+                insert({ key, Value() });
+                return (*find(key)).second;
+            }
+        }
+        bool contains(const Key& key) const {
+            Bucket* current = buckets[hash_function(key)];
+            while (current != nullptr) {
+                if (current->data.first == key) {
+                    return true;
+                }
+                current = current->next;
+            }
+            return false;
+        }
+        Iterator begin() {
+            for (size_t i = 0; i < hm_capacity; i++) {
+                if (buckets[i] != nullptr) {
+                    return Iterator(buckets[i], buckets, i, hm_capacity);
+                }
+            }
+            return end();
+        }
+
+        Iterator end() {
+            return Iterator(nullptr, buckets, hm_capacity, hm_capacity);
+        }
+
+        const size_t size() const {
+            return hm_size;
+        }
+
+        void clear() {
+            if (buckets == nullptr) return;
+
+            for (size_t i = 0; i < hm_capacity; i++) {
+                Bucket* current = buckets[i];
+                while (current != nullptr) {
+                    Bucket* temp = current;
+                    current = current->next;
+                    delete temp;
+                }
+                buckets[i] = nullptr;
+            }
+            hm_size = 0;
+        }
+
+        bool empty() {
+            return !buckets;
+        }
+
+        static void ruleoffive_hashmap_demo() {
+            my_std::Hashmap<std::string, int> hm_example = {
+                {"current year", 2025},
+                {"current month", 12},
+                {"hours in day", 24},
+                {"Google company formation", 1998}
+            };
+            std::cout << "" << std::endl;
+            my_std::Hashmap<std::string, int> hm_example_c_copy(hm_example);
+            std::cout << "" << std::endl;
+            my_std::Hashmap<std::string, int> hm_example_c_move = std::move(hm_example_c_copy);
+            std::cout << "" << std::endl;
+            my_std::Hashmap<std::string, int> hm_example_op_cas;
+            std::cout << "" << std::endl;
+            hm_example_op_cas = hm_example_c_move;
+            std::cout << "" << std::endl;
+            std::cout << "" << std::endl;
         }
     };
 
@@ -149,6 +299,7 @@ namespace my_std {
     template<typename Key, typename Value>
     class unordered_map {
     private:
+        using Iterator = typename Hashmap<const Key, Value>::Iterator;
         using value_type = std::pair<const Key, Value>;
         Hashmap<const Key, Value> hm;
     public:
@@ -164,14 +315,57 @@ namespace my_std {
         void erase(const Key& pair) {
             hm.remove(pair);
         }
-    };
 
+        Value& operator[](const Key& key) {
+            if (contains(key)) {
+                return (*find(key)).second;
+            }
+            else {
+                insert({ key, Value() });
+                return (*find(key)).second;
+            }
+        }
+        Iterator find(const Key& pair) {
+            return hm.search(pair);
+        }
+
+        bool contains(const Key& pair) const {
+            return hm.contains(pair);
+        }
+        Iterator begin() {
+            return hm.begin();
+        }
+        Iterator end() {
+            return hm.end();
+        }
+
+        const size_t size() const {
+            return hm.size();
+        }
+
+        void clear() {
+            hm.clear();
+        }
+
+        bool empty() {
+            return hm.empty();
+        }
+
+
+
+
+        static void ruleoffive_unordered_map_demo() {
+            Hashmap<std::string, std::string>::ruleoffive_hashmap_demo();
+        }
+    };
 };
 
 
 int main()
 {
     std::cout << "Welcome to my_std_unordered_map! There is an example of unordered_map<const Key, Value> on hashmap with chaining." << std::endl;
+
+    my_std::unordered_map<std::string, std::string>::ruleoffive_unordered_map_demo();
 
     my_std::unordered_map<std::string, std::string> my_unordered_map = {
         {"Car", "BMW"},
@@ -185,5 +379,8 @@ int main()
     my_unordered_map.insert({ "Website", "Google" });
     std::cout << "unordered_map.insert('Cra', 'Toyota');  (create a collision)" << std::endl;
     my_unordered_map.insert({ "Cra", "Toyota" });
+
+    std::cout << "unordered_map.find('Cra'); " << std::endl;
+    std::cout << "Address of this key: " << (*my_unordered_map.find("Cra")) << ". " << std::endl;
     return 0;
 }
